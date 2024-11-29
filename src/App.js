0df4ac213cb22d32fa822f58d4a1ecd97f7f027a
girishmaharna42@gmail.com <girishmaharana42@gmail.com>
2024-11-30 00:06:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar.tsx";
+import { BrowserRouter, Routes ,Route} from "react-router-dom";
+import Home from "./Pages/Home.tsx";
+import MovieList from "./Pages/MovieList.tsx";
+import MovieDetails from "./Pages/MovieDetails.tsx";
+import Toprated from "./Pages/Toprated.jsx";
+import Actordetails from "./Pages/Actordetails.tsx";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-black text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/movies" element={<MovieList/>}/>
+          <Route path="/movie/:id" element={<MovieDetails/>}/>
+          <Route path="/actor/:id" element={<Actordetails/>}/>
+          <Route path="/top-rated" element={<Toprated/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
